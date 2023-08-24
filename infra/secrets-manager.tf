918d1df9d683e6973ba0e806e320591793359901
rav94 <fernandoravindu1994@gmail.com>
2023-08-24 21:03:38 +0530

    Removing wrong params

--- infra/secrets-manager.tf
+++ infra/secrets-manager.tf
@@ -1,5 +1,4 @@
 resource "aws_secretsmanager_secret" "dynamodb-table-name" {
   name                    = "DynamoDB-Table-Name"
-  value                   = "UsersTable-${var.env}"
   recovery_window_in_days = "0"
 }
