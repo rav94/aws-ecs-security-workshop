513326edef92a855ef5093bc5ddfe1cd506e6788
rav94 <fernandoravindu1994@gmail.com>
2023-08-24 23:08:03 +0530

    Adding open egress traffic to Fargate security group

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -70,10 +70,11 @@ resource "aws_security_group" "allow-ecs-fargate-task" {
   description = "security group for ecs fargate task"
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
